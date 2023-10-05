6bd09b970eab005f0572f8de42201cf1c4202a17
keith-kay <keithrhova@gmail.com>
2023-10-05 09:51:22 +0000

    updt

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Defines annotated variables"""
-
+""" Define variables """
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
+school: str = "Holberton"
