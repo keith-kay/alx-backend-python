84386089cb423f432b924d01331438d884f69c3c
keith-kay <keithrhova@gmail.com>
2023-10-05 12:08:48 +0000

    updt

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
-""" Complex types - list of floats """
-from typing import Callable, Iterator, Union, Optional, List
+"""
+    List of floats annotations
+"""
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
     """
+        Args:
+            input_list: float numbers
 
+        Return:
+            Sum of the float numbers
+    """
+
+    result: float = 0
+
+    for x in input_list:
+        result += x
+
+    return result
