0918d3bac683a42055b1c3a44e83628e077b285e
keith-kay <keithrhova@gmail.com>
2025-05-25 22:07:35 +0300

    last commit

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,29 @@
+import asyncio
+import aiosqlite
+
+async def async_fetch_users():
+    async with aiosqlite.connect('users.db') as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            users = await cursor.fetchall()
+            print("All users:")
+            for user in users:
+                print(user)
+            return users
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect('users.db') as db:
+        async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            users = await cursor.fetchall()
+            print("Users older than 40:")
+            for user in users:
+                print(user)
+            return users
+
+async def fetch_concurrently():
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+
+if __name__ == "__main__":
+    asyncio.run(fetch_concurrently())
