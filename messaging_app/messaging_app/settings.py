c851409b19b489a566c575681652bfa09ff86b2c
keith-kay <keithrhova@gmail.com>
2025-06-01 23:09:33 +0300

    updt

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -126,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        # You can also add JWT or Token auth here later
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
