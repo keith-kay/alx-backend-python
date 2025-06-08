71745f563942e0eac168276ec2e17eb242b9da94
keith-kay <keithrhova@gmail.com>
2025-06-08 22:32:33 +0300

    updt 1

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -8,10 +8,15 @@ class UserSerializer(serializers.ModelSerializer):
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_preview = serializers.CharField(source='message_body', read_only=True)
+    sent_at_formatted = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
-        fields = ['message_id', 'conversation', 'sender', 'message_body', 'sent_at']
+        fields = ['message_id', 'conversation', 'sender', 'message_body', 'sent_at', 'message_preview', 'sent_at_formatted']
+
+    def get_sent_at_formatted(self, obj):
+        return obj.sent_at.strftime('%Y-%m-%d %H:%M:%S')
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
@@ -20,3 +25,8 @@ class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Conversation
         fields = ['conversation_id', 'participants', 'created_at', 'messages']
+
+    def validate(self, data):
+        if 'participants' in data and len(data['participants']) < 2:
+            raise serializers.ValidationError("A conversation must have at least two participants.")
+        return data
