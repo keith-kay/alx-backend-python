7d561a668da56f7fc8a6af9972cbef6e002e59e1
keith-kay <keithrhova@gmail.com>
2025-06-15 22:17:59 +0300

    updt

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -5,4 +5,10 @@ class Message(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
-    timestamp =
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, related_name='notifications', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
