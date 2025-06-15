c459cb95126a3e2da4192eecf4da0976534df317
keith-kay <keithrhova@gmail.com>
2025-06-15 22:46:18 +0300

    updt

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,5 +1,6 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
@@ -13,7 +14,7 @@ def create_notification_on_message(sender, instance, created, **kwargs):
 @receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, **kwargs):
     if not instance.pk:
-        return  # Skip new messages
+        return
     try:
         old_message = Message.objects.get(pk=instance.pk)
     except Message.DoesNotExist:
@@ -21,6 +22,13 @@ def log_message_edit(sender, instance, **kwargs):
     if old_message.content != instance.content:
         MessageHistory.objects.create(
             message=instance,
-            old_content=old_message.content
+            old_content=old_message.content,
+            edited_by=instance.sender
         )
         instance.edited = True
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+    Notification.objects.filter(user=instance).delete()
