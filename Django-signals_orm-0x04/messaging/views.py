156578f56b8258e490664b673dd87a175f2ee1b7
keith-kay <keithrhova@gmail.com>
2025-06-15 23:02:54 +0300

    updt

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -77,5 +77,7 @@ class Message(models.Model):
 
 @login_required
 def inbox(request):
-    unread_messages = Message.unread.unread_for_user(request.user)
+    unread_messages = Message.unread.unread_for_user(request.user).only(
+        "id", "sender", "content", "timestamp"
+    )
     return render(request, "messaging/inbox.html", {"unread_messages": unread_messages})
